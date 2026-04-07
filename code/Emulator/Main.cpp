4d3015cd00235b64ba236ed05331815d9867a991
Anders Pistol <anders@pistol.one>
2026-04-07 23:01:17 +0200

    Using Wayland UI instead of X11.

--- code/Emulator/Main.cpp
+++ code/Emulator/Main.cpp
@@ -38,7 +38,7 @@
 #elif defined(__APPLE__)
 #	include <Ui/Cocoa/WidgetFactoryCocoa.h>
 #elif defined(__LINUX__) || defined(__RPI__)
-#	include <Ui/X11/WidgetFactoryX11.h>
+#	include <Ui/Wl/WidgetFactoryWl.h>
 #endif
 
 //
@@ -87,7 +87,7 @@ int main(int argc, const char** argv)
 	);
 #elif defined(__LINUX__) || defined(__RPI__)
 	ui::Application::getInstance()->initialize(
-		new ui::WidgetFactoryX11(),
+		new ui::WidgetFactoryWl(),
 		nullptr
 	);
 #endif
